4748074f19e0c41d9e6d714b36792e30e6a0a71a
Arne <arecknag@gmail.com>
2019-12-01 22:17:29 +0100

    re-ran black

--- day1.py
+++ day1.py
@@ -7,18 +7,18 @@ def get_input(loc, func: Callable = str, strip=True):
 
 
 def part_1(data):
-    return sum(val//3-2 for val in data)
+    return sum(val // 3 - 2 for val in data)
 
 
 def part_2(data):
     acc = 0
     for val in data:
-        while (val := val//3-2) > 0:
+        while (val := val // 3 - 2) > 0:
             acc += val
     return acc
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     data_ = get_input("data/day1", func=int)
 
     res_1 = part_1(data_)
