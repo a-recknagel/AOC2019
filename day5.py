aaf24254419fd62d139b55c179cae0f78b878d36
Arne <arecknag@gmail.com>
2019-12-08 01:27:37 +0100

    day 7 part two
    changes to IntCode because my current idx update model lead
    to too many bugs. still running into endless loops, but better
    than getting off by ones all over the place.

--- day5.py
+++ day5.py
@@ -1,4 +1,7 @@
 import typing as ty
+from collections import deque
+from itertools import islice
+
 
 from util import get_input
 
@@ -7,12 +10,16 @@ class Finished(Exception):
     pass
 
 
+class NoInput(Exception):
+    pass
+
+
 class IntCode:
     def __init__(self, program: ty.List[int], input: ty.Iterable[int]):
         self.program = program.copy()
-        self.input = iter(input)
+        self.input = deque(input)
         self.idx = 0
-        self.output: ty.List[int] = []
+        self.output = deque()
         self.op_code_map = {
             1: self.add,
             2: self.mul,
@@ -25,58 +32,68 @@ class IntCode:
             99: self.die,
         }
 
-    def fetch(self, mode):
-        if mode:
-            ret = self.idx
+    def resolve_idx(self, offset, mode):
+        if mode[offset - 1]:  # instructions progress one further than modes
+            ret = self.idx + offset
         else:
-            ret = self.program[self.idx]
-        self.idx += 1
+            ret = self.program[self.idx + offset]
         return ret
 
     def add(self, modes):
-        noun = self.program[self.fetch(next(modes))]
-        verb = self.program[self.fetch(next(modes))]
-        self.program[self.fetch(next(modes))] = noun + verb
+        noun = self.program[self.resolve_idx(1, modes)]
+        verb = self.program[self.resolve_idx(2, modes)]
+        self.program[self.resolve_idx(3, modes)] = noun + verb
+        self.idx += 4
 
     def mul(self, modes):
-        noun = self.program[self.fetch(next(modes))]
-        verb = self.program[self.fetch(next(modes))]
-        self.program[self.fetch(next(modes))] = noun * verb
+        noun = self.program[self.resolve_idx(1, modes)]
+        verb = self.program[self.resolve_idx(2, modes)]
+        self.program[self.resolve_idx(3, modes)] = noun * verb
+        self.idx += 4
 
     def mov(self, modes):
-        self.program[self.fetch(next(modes))] = next(self.input)
+        if not self.input:
+            raise NoInput
+        self.program[self.resolve_idx(1, modes)] = self.input.popleft()
+        self.idx += 2
 
     def out(self, modes):
-        self.output.append(self.program[self.fetch(next(modes))])
+        self.output.append(self.program[self.resolve_idx(1, modes)])
+        self.idx += 2
 
     def jit(self, modes):
-        cond = self.program[self.fetch(next(modes))]
-        loc = self.program[self.fetch(next(modes))]
+        cond = self.program[self.resolve_idx(1, modes)]
+        loc = self.program[self.resolve_idx(2, modes)]
         if cond:
             self.idx = loc
+        else:
+            self.idx += 3
 
     def jif(self, modes):
-        cond = not self.program[self.fetch(next(modes))]
-        loc = self.program[self.fetch(next(modes))]
+        cond = not self.program[self.resolve_idx(1, modes)]
+        loc = self.program[self.resolve_idx(2, modes)]
         if cond:
             self.idx = loc
+        else:
+            self.idx += 3
 
     def lt(self, modes):
-        noun = self.program[self.fetch(next(modes))]
-        verb = self.program[self.fetch(next(modes))]
-        self.program[self.fetch(next(modes))] = int(noun < verb)
+        noun = self.program[self.resolve_idx(1, modes)]
+        verb = self.program[self.resolve_idx(2, modes)]
+        self.program[self.resolve_idx(3, modes)] = int(noun < verb)
+        self.idx += 4
 
     def eq(self, modes):
-        noun = self.program[self.fetch(next(modes))]
-        verb = self.program[self.fetch(next(modes))]
-        self.program[self.fetch(next(modes))] = int(noun == verb)
+        noun = self.program[self.resolve_idx(1, modes)]
+        verb = self.program[self.resolve_idx(2, modes)]
+        self.program[self.resolve_idx(3, modes)] = int(noun == verb)
+        self.idx += 4
 
     def die(self, _):
         raise Finished
 
     def parse_instructions(self):
         instruction = str(self.program[self.idx])
-        self.idx += 1
         if len(instruction) == 1:  # pad
             instruction = f"0{instruction}"
         yield int(instruction[-2:])
@@ -84,12 +101,14 @@ class IntCode:
         while True:
             yield 0
 
+    def step(self):
+        op_code, *modes = islice(self.parse_instructions(), 6)  # there can't be too many modes
+        self.op_code_map[op_code](modes)
+
     def run(self):
         while True:
-            instructions = self.parse_instructions()
-            op_code = next(instructions)
             try:
-                self.op_code_map[op_code](instructions)
+                self.step()
             except Finished:
                 return self.output
 
@@ -109,21 +128,21 @@ def part_2():
     test_2_1 = IntCode([
         3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
         1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
-        999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99], 7).run()
+        999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99], [7]).run()[0]
     print(f"{test_2_1=}")
     test_2_2 = IntCode([
         3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
         1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
-        999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99], 8).run()
+        999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99], [8]).run()[0]
     print(f"{test_2_2=}")
     test_2_3 = IntCode([
         3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
         1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
-        999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99], 9).run()
+        999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99], [9]).run()[0]
     print(f"{test_2_3=}")
 
     data = list(map(int, get_input("data/day5")[0].split(',')))
-    res_2 = IntCode(data, 5).run()
+    res_2 = IntCode(data, [5]).run()[0]
     print(f"{res_2=}")
 
 
